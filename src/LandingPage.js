5ed4e1bad6c215cdf3b3e7cb30ea8d5ab764b86d
Pato Gomez <Pato_Gomez@Patos-MacBook-Pro-2.local>
2023-03-20 16:56:55 -0600

    testing

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -5,7 +5,7 @@ import BottomLine from './components/BottomLine';
 
 function LandingPage() {
   return (
-    <><div className="flex flex-col items-center justify-center h-screen bg-gradient-br-tl">
+    <><div className="flex flex-col items-center justify-center h-screen bg-gradient-br-tl overflow-y-auto">
       <div className="flex relative items-center justify-center rounded-full h-[256px] w-[256px] bg-gradient-purple -mt-[100px]">
         <div className="flex relative items-center justify-start rounded-full h-[240px] w-[240px] bg-white">
           <div className="flex items-center justify-center">
@@ -21,14 +21,14 @@ function LandingPage() {
           </div>
         </div>
       </div>
-      <h2 className="text-4xl mt-16 font-bold md:text-[50px]">
+      <h2 className="text-3xl mt-16 font-bold md:text-[50px]">
         <span className="text-myPurple">Badgers</span><span className="text-black">Build Discount</span>
       </h2>
       <div className='flex flex-col items-center mt-5'>
-      <p className="mx-3 text-center font-light mt-2 md:text-[26px] text-[#424242]">
+      <p className="mx-3 text-center text-sm font-light mt-2 md:text-[26px] text-[#424242]">
         Built by Badgers for the future builders - login & stake Badger 
         </p>
-        <p className="mx-3 text-center font-light md:text-[26px] text-[#424242]">
+        <p className="mx-3 text-center text-sm font-light md:text-[26px] text-[#424242]">
         NFTs to claim a 50% discount for the <a href="https://clarity11.teachable.com/" target="_blank" rel="noreferrer" className="text-myPurple hover:underline">Clarity Course.</a>
         </p>
       </div>
