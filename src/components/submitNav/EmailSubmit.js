3b692eeefb9cc1e9e091ab883d66a2c4f9dc4736
Bernardo Garciarivas <brgarciarivas@gmail.com>
2023-03-22 10:23:01 -0400

    add api route

--- src/components/submitNav/EmailSubmit.js
+++ src/components/submitNav/EmailSubmit.js
@@ -1,13 +1,16 @@
 import React, { useState } from "react";
+import { GraphQLClient, gql } from "graphql-request";
 
 function EmailSubmit() {
   const [email, setEmail] = useState("");
   const [isValid, setIsValid] = useState(true);
 
-    const handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     if (validateEmail(email)) {
       console.log("Email is valid:", email);
+
+      handleAddEmail("0xTEST0X", email);
       // perform any necessary actions with valid email
     } else {
       setIsValid(false);
@@ -24,20 +27,49 @@ function EmailSubmit() {
     return regex.test(email);
   };
 
+  const handleAddEmail = async (senderAddress = "", email = "") => {
+    try {
+      const endpoint = "https://protected-oasis-02113.herokuapp.com/graphql";
+
+      const graphQLClient = new GraphQLClient(endpoint, {
+        headers: {
+          authorization: "",
+        },
+      });
+
+      const query = gql`
+        {
+          stakingDiscount(txId: "${senderAddress}", email: "${email}") 
+        }
+      `;
+
+      const data = await graphQLClient.request(query);
+      console.log(" handleAddEmail data", data);
+      if (data.handleAnonCli) {
+        if (data.handleAnonCli === "False") {
+          window.alert("ERR_PLZ_REPORT_TO_MONEKY_DEV");
+        } else {
+        }
+      }
+    } catch (err) {
+      console.log("err", err);
+    }
+  };
+
   return (
     <div className="">
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="email"></label>
-      <input
-        type="text"
-        id="email"
-        placeholder="test@test.com"
-        className="border border-purple-600 rounded-full py-1 text-center font-bold text-black mb-1.5 mt-4"
-        value={email}
-        onChange={handleEmailChange}
-      />
-      {!isValid && <span>!</span>}
-    </form>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email"></label>
+        <input
+          type="text"
+          id="email"
+          placeholder="test@test.com"
+          className="border border-purple-600 rounded-full py-1 text-center font-bold text-black mb-1.5 mt-4"
+          value={email}
+          onChange={handleEmailChange}
+        />
+        {!isValid && <span>!</span>}
+      </form>
     </div>
   );
 }
